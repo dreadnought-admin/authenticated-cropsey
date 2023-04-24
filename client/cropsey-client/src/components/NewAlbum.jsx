36dc668ac9fc8531942f76d02ad45677b709dd3b
dom alexander <dom.alexan@gmail.com>
2023-04-24 13:53:06 -0600

    Edits album release date and spotify link

--- client/cropsey-client/src/components/NewAlbum.jsx
+++ client/cropsey-client/src/components/NewAlbum.jsx
@@ -9,8 +9,8 @@ const NewAlbum = () => {
     </div>
     <div className="new-album-text">
         <h1>MALFEASANCE</h1>
-        <h2>FULL ALBUM DROPS APRIL 21ST</h2>
-        <a target="_blank" href="https://open.spotify.com/track/09PEJ12EumieKJZZrYEACL?si=f235856f6fdd4416"><button>LISTEN ON SPOTIFY</button></a>
+        <h2>FULL ALBUM OUT NOW</h2>
+        <a target="_blank" href="https://open.spotify.com/album/0sOaKqZFbBGNHncgUQReLx"><button>LISTEN ON SPOTIFY</button></a>
     </div>
     {/* <div className="clear"></div> */}
     </div>
