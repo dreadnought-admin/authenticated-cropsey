33f23211e415e84c7d0ade6021ac6a766b50efac
dom alexander <dom.alexan@gmail.com>
2023-04-24 14:44:05 -0600

    Begins setup for deployment

--- server/cropsey-api/config/routes.rb
+++ server/cropsey-api/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # get "/authorized", to: "sessions#show"
   # get "/dashboard", to: "users#show"
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+
   resources :news_posts
   resources :music_posts
   resources :tour_dates
